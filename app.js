b1dd8a840e983101cf9d0bad3c6c4a4539d3566c
Peter Hristakos <peter.hristakos@bigpond.com>
2020-04-07 12:31:43 +1000

    ready to deploy

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// timer API - built in Library in browser.
+
+let startBtn = document.querySelector(".start-btn");
+let pauseBtn = document.querySelector(".pause-btn");
+let resetBtn = document.querySelector(".reset-btn");
+let displayTimer = document.querySelector(".display-timer");
+
+let timerId = null;
+let seconds = 0;
+
+let tick = function () {
+    seconds++;
+    displayTimer.textContent = seconds;
+}
+
+
+let handleStart = function () {
+    if (timerId) {
+        return;
+    }
+    timerId = setInterval(tick, 1000);
+}
+
+let handleReset = function () {
+    clearInterval(timerId);
+    displayTimer.textContent = 0;
+    timerId = null;
+}
+
+let handlePause = function () {
+    clearInterval(timerId);
+    timerId = null;
+};
+
+
+startBtn.addEventListener('click', handleStart);
+pauseBtn.addEventListener('click', handlePause)
+resetBtn.addEventListener('click', handleReset)
